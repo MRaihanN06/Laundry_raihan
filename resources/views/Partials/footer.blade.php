72bbe1718d972fef8c314320ed90e9de531f7fb7
MRaihanN06 <muhamadraihannugraha15@gmail.com>
2022-01-31 15:14:15 +0700

    Template

---
+++ resources/views/Partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©2021. <a href="#" target="_blank">Laundry MRaihanN</a></span>
+            <span class="text-muted text-center text-sm-right d-block mt-1 mt-sm-0 d-sm-inline-block">Dibuat Oleh <a href="#" target="_blank">Muhamad Raihan Nugraha</a></span> 
+          </div>
+        </footer> 
