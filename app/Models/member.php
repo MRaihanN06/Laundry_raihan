a0692545b317a0375edf3d755333662b41149ea2
MRaihanN06 <muhamadraihannugraha15@gmail.com>
2022-02-01 19:32:43 +0700

    Controller dan Model Member

---
+++ app/Models/member.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class member extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+    protected $table = 'tb_member';
+    protected $fillable = [
+        'nama',
+        'alamat',
+        'jenis_kelamin',
+        'tlp'
+    ];
+}
