cc1791f113521f0f49d569f6fd896a9ec7cb8202
MRaihanN06 <muhamadraihannugraha15@gmail.com>
2022-02-01 20:39:48 +0700

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,12 +24,47 @@ class AuthController extends Controller
   
     public function login(Request $request)
     {
-        
+        $rules = [
+            'email'                 => 'required|email',
+            'password'              => 'required|string'
+        ];
+  
+        $messages = [
+            'email.required'        => 'Email wajib diisi',
+            'email.email'           => 'Email tidak valid',
+            'password.required'     => 'Password wajib diisi',
+            'password.string'       => 'Password harus berupa string'
+        ];
+  
+        $validator = Validator::make($request->all(), $rules, $messages);
+  
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput($request->all);
+        }
+  
+        $data = [
+            'email'     => $request->input('email'),
+            'password'  => $request->input('password'),
+        ];
+  
+        Auth::attempt($data);
+  
+        if (Auth::check()) { // true sekalian session field di users nanti bisa dipanggil via Auth
+            //Login Success
+            return redirect()->route('home');
+  
+        } else { // false
+  
+            //Login Fail
+            Session::flash('error', 'Email atau password salah');
+            return redirect()->route('login');
+        }
+  
     }
   
     public function showFormRegister()
     {
-        
+        return view('register');
     }
   
     public function register(Request $request)
@@ -39,7 +74,7 @@ class AuthController extends Controller
   
     public function logout()
     {
-       
+        
     }
   
   
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -33,6 +33,29 @@
                 <form action="{{ route('login') }}" method="post">
                 @csrf
                 <div class="card-body">
+                    @if(session('errors'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            Something it's wrong:
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                            <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @if (Session::has('success'))
+                        <div class="alert alert-success">
+                            {{ Session::get('success') }}
+                        </div>
+                    @endif
+                    @if (Session::has('error'))
+                        <div class="alert alert-danger">
+                            {{ Session::get('error') }}
+                        </div>
+                    @endif
                     <div class="form-group">
                         <label for=""><strong>Email</strong></label>
                         <input type="text" name="email" class="form-control" placeholder="Email">
