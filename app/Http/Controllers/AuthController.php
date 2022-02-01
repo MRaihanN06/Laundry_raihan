3090bbff4535c4b390cb48107f3e05d7cf114162
MRaihanN06 <muhamadraihannugraha15@gmail.com>
2022-02-01 20:52:34 +0700

    Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,7 +69,43 @@ class AuthController extends Controller
   
     public function register(Request $request)
     {
-       
+        $rules = [
+            'name'                  => 'required|min:3|max:35',
+            'email'                 => 'required|email|unique:users,email',
+            'password'              => 'required|confirmed'
+        ];
+  
+        $messages = [
+            'name.required'         => 'Nama Lengkap wajib diisi',
+            'name.min'              => 'Nama lengkap minimal 3 karakter',
+            'name.max'              => 'Nama lengkap maksimal 35 karakter',
+            'email.required'        => 'Email wajib diisi',
+            'email.email'           => 'Email tidak valid',
+            'email.unique'          => 'Email sudah terdaftar',
+            'password.required'     => 'Password wajib diisi',
+            'password.confirmed'    => 'Password tidak sama dengan konfirmasi password'
+        ];
+  
+        $validator = Validator::make($request->all(), $rules, $messages);
+  
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput($request->all);
+        }
+  
+        $user = new User;
+        $user->name = ucwords(strtolower($request->name));
+        $user->email = strtolower($request->email);
+        $user->password = Hash::make($request->password);
+        $user->email_verified_at = \Carbon\Carbon::now();
+        $simpan = $user->save();
+  
+        if($simpan){
+            Session::flash('success', 'Register berhasil! Silahkan login untuk mengakses data');
+            return redirect()->route('login');
+        } else {
+            Session::flash('errors', ['' => 'Register gagal! Silahkan ulangi beberapa saat lagi']);
+            return redirect()->route('register');
+        }
     }
   
     public function logout()
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -33,6 +33,19 @@
           <form action="{{ route('register') }}" method="post">
           @csrf
           <div class="card-body">
+              @if(session('errors'))
+                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                      Something it's wrong:
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">×</span>
+                      </button>
+                      <ul>
+                      @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                      @endforeach
+                      </ul>
+                  </div>
+              @endif
               <div class="form-group">
                   <label for=""><strong>Nama Lengkap</strong></label>
                   <input type="text" name="name" class="form-control" placeholder="Nama Lengkap">
